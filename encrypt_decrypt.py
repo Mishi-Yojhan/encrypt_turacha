dc992ebef947c600ae010d3f9d1f11e6d5222ad6
Yojhan Barrios <yojhan.official@gmail.com>
2021-09-20 20:07:21 -0500

    Declaración métodos encrypt y decrypt
    Se declaran los métodos encrypt y decrypt encargados de encriptar y desencriptar respectivamente.

---
+++ encrypt_decrypt.py
@@ -0,0 +1,32 @@
+
+def encrypt(cadena: str) -> str:
+    """Función encargada de encriptar la cadena ingresada
+
+    Args:
+        cadena (str): Cadena a ingresar
+
+    Returns:
+        str: Cadena ya encriptada
+    """
+
+    asciiConcatenado: str = "" 
+    cadenaEncriptada: str = "" # Espacio para cadena ya encriptada
+
+    return cadenaEncriptada
+
+
+def decrypt(cadenaEncriptada: str) -> str:
+    """Desencriptar
+
+    Args:
+        cadenaEncriptada (str): Cadena a desencriptar
+
+    Returns:
+        str: cadena desencriptada
+    """
+
+    cadenaDesencriptada: str = "" # Espacio para la cadena ya desencriptada
+
+    # TO DO: Proceso desencriptar
+
+    return cadenaDesencriptada # Retorno cadena desencriptada
